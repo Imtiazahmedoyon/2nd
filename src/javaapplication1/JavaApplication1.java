12a03acf8014e43e3a0f9c1827e6f19d3a95ac0c
Imtiazahmedoyon <ahmed2305101942@diu.edu.bd>
2024-10-22 13:10:45 +0600

    Update JavaApplication1.java

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -9,7 +9,7 @@ public class JavaApplication1 {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("hello world");
+        System.out.print("hello world pogjkeryhop");
     }
     
 }
